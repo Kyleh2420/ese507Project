4dac92fed9e57dddcfddea0e011a89887ce3a611
KyleH2420 <kyleh2420@gmail.com>
2024-12-04 18:23:55 -0500

    Copied to prepare for optimizations

---
+++ 5-Optimizing/MMM.sv
@@ -0,0 +1,248 @@
+module MMM #(
+        parameter INW = 12,
+        parameter OUTW = 32,
+        parameter M = 7,
+        parameter N = 9,
+        parameter MAXK = 8,
+        localparam K_BITS = $clog2(MAXK+1)
+    )(
+        input clk,
+        input reset,
+        input [INW-1:0] INPUT_TDATA,
+        input INPUT_TVALID,
+        input [K_BITS:0] INPUT_TUSER,
+        output INPUT_TREADY,
+        output [OUTW-1:0] OUTPUT_TDATA,
+        output OUTPUT_TVALID,
+        input OUTPUT_TREADY
+    );
+
+
+    //Named nets between the modules, to help keep things organized
+    //Utilized to "pipe" data around, controlled by the overall FSM
+
+    //input_mems logic
+    logic matricesLoaded, computeFinished;
+    logic [K_BITS-1:0] K;
+    logic [$clog2(M*MAXK)-1:0] aAddress;
+    logic [$clog2(MAXK*N)-1:0] bAddress;
+    logic signed [INW-1:0] aData, bData;
+
+    //mac_pipe logic
+    logic signed [OUTW-1:0] macOut;
+    logic clearAcc, validInput;
+
+    //fifo logic
+    logic fifoWriteEnable;
+    logic [$clog2(N+1)-1:0] fifoCapacity;
+
+    //State Logic
+    enum {waitForLoad, compute, stall} currentState, nextState;
+
+    //Internal signals used to determine current status
+    logic [K_BITS-1:0] index;
+    logic [M-1:0] outputRow;
+    logic [N-1:0] outputCol;
+
+    input_mems #(INW, M, N, MAXK) inputMem(
+        .clk(clk),
+        .reset(reset),
+        .AXIS_TDATA(INPUT_TDATA),
+        .AXIS_TVALID(INPUT_TVALID),
+        .AXIS_TUSER(INPUT_TUSER),
+        .AXIS_TREADY(INPUT_TREADY),
+        .matrices_loaded(matricesLoaded),
+        .compute_finished(computeFinished),
+        .K(K),
+        .A_read_addr(aAddress),
+        .A_data(aData),
+        .B_read_addr(bAddress),
+        .B_data(bData)
+    );
+
+    mac_pipe #(INW, OUTW) mac(
+        .in0(aData), 
+        .in1(bData),
+        .out(macOut),
+        .clk(clk), 
+        .reset(reset), 
+        .clear_acc(clearAcc),
+        .valid_input(validInput)
+    );
+
+    fifo_out #(OUTW, N) fifo(
+        .clk(clk),
+        .reset(reset),
+        .data_in(macOut),
+        .wr_en(fifoWriteEnable),
+        .capacity(fifoCapacity),
+        .AXIS_TDATA(OUTPUT_TDATA),
+        .AXIS_TVALID(OUTPUT_TVALID),
+        .AXIS_TREADY(OUTPUT_TREADY)
+    );
+
+    //Used to delay the clear accumulate signal by one additional clock
+    logic clearAccDelay1, clearAccDelay2, fifoWriteEnableDelay1, fifoWriteEnableDelay2, validDelay1;
+    always_ff @(posedge clk) begin : delayAccumulateClear
+
+        clearAcc <= clearAccDelay1;
+        clearAccDelay1 <= clearAccDelay2;
+        fifoWriteEnable <= fifoWriteEnableDelay1;
+        fifoWriteEnableDelay1 <= fifoWriteEnableDelay2;
+        // validInput <= validDelay1;
+    end
+
+    //Datapath stuff
+    always_comb begin: dataPath
+
+        case (currentState)
+            waitForLoad: begin
+                aAddress = 0;
+                bAddress = 0;
+            end
+            compute: begin
+                aAddress = index + (outputRow * K);
+                bAddress = (index * N) + outputCol;
+            end
+            stall: begin
+                aAddress = index + (outputRow * K);
+                bAddress = (index * N) + outputCol;
+            end
+            default: begin
+                //Removing the inferred latch
+                aAddress = 0;
+                bAddress = 0;
+            end
+        endcase
+    end
+
+    //NextState Logic
+    always_comb begin : nextStateLogic
+        if (reset == 1) begin
+            nextState <= waitForLoad;
+        end else begin
+            case (currentState)
+            waitForLoad: begin
+                if (matricesLoaded == 1) begin
+                    nextState <= compute;
+                end else begin
+                    nextState <= waitForLoad;
+                end
+            end
+            compute: begin
+                //As long as we can write to the outputFIFO, continue computing. Unless it is full, then move to state stall
+                //We also count as full if the capacity is 1 and we're about to write to the fifo (Check fifoWriteDelay and fifoWriteDelay1)
+                if (fifoCapacity == 0 || ((fifoWriteEnable == 1 || fifoWriteEnableDelay1 == 1) && fifoCapacity == 1)) begin
+                    //There is no space in the fifo -- stall
+                    nextState <= stall;
+                end else begin
+                    if (computeFinished) begin
+                        nextState <= waitForLoad;
+                    end else begin
+                        nextState <= compute;
+                    end
+                end
+            end
+            stall: begin
+                if (fifoCapacity == 0 || ((fifoWriteEnable == 1 || fifoWriteEnableDelay1 == 1) && fifoCapacity == 1)) begin
+                    //Fifo is full -- continue stalling
+                    nextState <= stall;
+                end else begin
+                    //Fifo is no longer full -- go back to computing
+                    nextState <= compute;
+                end
+            end
+            default: begin
+                //Removing the inferred latch
+                nextState <= waitForLoad;
+            end
+            endcase
+        end
+    end
+
+    always_ff @( posedge clk ) begin : controlSignals
+
+    currentState <= nextState;
+
+        //Defaults for the control signals
+        computeFinished <= 0;
+        index <= index;
+        outputRow <= outputRow;
+        outputCol <= outputCol;
+        fifoWriteEnableDelay2 <= 0;
+        clearAccDelay2 <= 0;
+        validInput <= 0;
+
+        case (currentState)
+            waitForLoad: begin
+                index <= 0;
+                outputCol <= 0;
+                outputRow <= 0;
+                computeFinished <= 0;
+            end
+            compute: begin
+                if (index == K-1) begin
+                    //In this block, we are done computing the dot product.
+                    //Choose something else to increment and reset K to the beginning
+                    if (outputCol == N-1) begin
+                        //In this block, we are done computing the entire output row. Increment to the next row, or check if we're done with the computation alltogether
+                        if (outputRow == M-1) begin
+                            //In this block, we're done computing the entire matrix. move to waitForFinish to ensure a response form input_mems
+                            
+
+                            //If the next state is waitForLoad, then this has been acknowleged. Drop computeFinished back to 0
+                            if (nextState == waitForLoad) begin
+                                computeFinished <= 0;
+                                fifoWriteEnableDelay2 <= 0;
+                                clearAccDelay2 <= 0;
+                                validInput <= 0;
+                            end else if (nextState != stall) begin
+                                //In addition, check to make sure we're not going to stall out 
+                                computeFinished <= 1;
+                                fifoWriteEnableDelay2 <= 1;
+                                clearAccDelay2 <= 1;
+                                validInput <= 1;
+                            end
+                        end else begin
+                            //In this block, we're not done computing the matrix. Increment to the next row
+
+                            //if nextState == stall, dont' do anything. Otherwise, continue as normal
+                            if (nextState != stall) begin
+                                index <= 0;
+                                outputRow <= outputRow + 1;
+                                outputCol <= 0;
+                                fifoWriteEnableDelay2 <= 1;
+                                clearAccDelay2 <= 1;
+                                validInput <= 1;
+                            end
+                        end
+                    end else begin
+                        //In this block, we're not yet done computing the output row. Increment outputCol.
+
+                        //If nextState == stall, don't do anything. Otherwise, continue as normal
+                        if (nextState != stall) begin
+                            index <= 0;
+                            outputRow <= outputRow;
+                            outputCol <= outputCol + 1;
+                            fifoWriteEnableDelay2 <= 1;
+                            clearAccDelay2 <= 1;
+                            validInput <= 1;
+                        end
+                    end
+                end else begin
+                    //Dot product is not done computing. Increment K and continue going.
+                    index <= index + 1;
+                    fifoWriteEnableDelay2 <= 0;
+                    clearAccDelay2 <= 0;
+                    validInput <= 1;
+                end
+            end
+            stall: begin
+                fifoWriteEnableDelay2 <= 0;
+                clearAccDelay2 <= 0;
+                validInput <= 0;
+            end
+        endcase
+    end
+
+endmodule
---
+++ 5-Optimizing/wave.do
@@ -0,0 +1,78 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate /MMM_tb/dut/clk
+add wave -noupdate /MMM_tb/errors
+add wave -noupdate /MMM_tb/dut/reset
+add wave -noupdate /MMM_tb/dut/currentState
+add wave -noupdate /MMM_tb/dut/nextState
+add wave -noupdate -divider -height 25 PARAMETERS
+add wave -noupdate /MMM_tb/dut/INW
+add wave -noupdate /MMM_tb/dut/OUTW
+add wave -noupdate /MMM_tb/dut/M
+add wave -noupdate /MMM_tb/dut/N
+add wave -noupdate /MMM_tb/dut/MAXK
+add wave -noupdate /MMM_tb/dut/K_BITS
+add wave -noupdate /MMM_tb/dut/K
+add wave -noupdate -divider -height 25 {DATA IN}
+add wave -noupdate -radix decimal /MMM_tb/dut/INPUT_TDATA
+add wave -noupdate /MMM_tb/dut/INPUT_TVALID
+add wave -noupdate /MMM_tb/dut/INPUT_TUSER
+add wave -noupdate /MMM_tb/dut/INPUT_TREADY
+add wave -noupdate -divider -height 25 {DATA OUT}
+add wave -noupdate -radix decimal /MMM_tb/dut/OUTPUT_TDATA
+add wave -noupdate /MMM_tb/dut/OUTPUT_TVALID
+add wave -noupdate /MMM_tb/dut/OUTPUT_TREADY
+add wave -noupdate -divider INPUT_MEMS
+add wave -noupdate /MMM_tb/dut/inputMem/aWriteEnable
+add wave -noupdate /MMM_tb/dut/inputMem/bWriteEnable
+add wave -noupdate -radix decimal -childformat {{{/MMM_tb/dut/inputMem/matrixA/mem[15]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[14]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[13]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[12]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[11]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[10]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[9]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[8]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[7]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[6]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[5]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[4]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[3]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[2]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[1]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixA/mem[0]} -radix decimal}} -subitemconfig {{/MMM_tb/dut/inputMem/matrixA/mem[15]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[14]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[13]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[12]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[11]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[10]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[9]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[8]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[7]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[6]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[5]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[4]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[3]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[2]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[1]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixA/mem[0]} {-height 16 -radix decimal}} /MMM_tb/dut/inputMem/matrixA/mem
+add wave -noupdate -radix decimal -childformat {{{/MMM_tb/dut/inputMem/matrixB/mem[15]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[14]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[13]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[12]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[11]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[10]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[9]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[8]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[7]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[6]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[5]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[4]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[3]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[2]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[1]} -radix decimal} {{/MMM_tb/dut/inputMem/matrixB/mem[0]} -radix decimal}} -subitemconfig {{/MMM_tb/dut/inputMem/matrixB/mem[15]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[14]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[13]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[12]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[11]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[10]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[9]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[8]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[7]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[6]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[5]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[4]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[3]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[2]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[1]} {-height 16 -radix decimal} {/MMM_tb/dut/inputMem/matrixB/mem[0]} {-height 16 -radix decimal}} /MMM_tb/dut/inputMem/matrixB/mem
+add wave -noupdate /MMM_tb/dut/inputMem/newA
+add wave -noupdate -divider {FIFO Stuff}
+add wave -noupdate -radix unsigned /MMM_tb/dut/fifoCapacity
+add wave -noupdate -radix decimal /MMM_tb/dut/fifoWriteEnable
+add wave -noupdate /MMM_tb/dut/fifoWriteEnableDelay1
+add wave -noupdate /MMM_tb/dut/fifoWriteEnableDelay2
+add wave -noupdate -radix decimal /MMM_tb/dut/fifo/myMemInst/mem
+add wave -noupdate -divider {ADDRESSING & DATA}
+add wave -noupdate /MMM_tb/dut/aAddress
+add wave -noupdate /MMM_tb/dut/bAddress
+add wave -noupdate -radix decimal /MMM_tb/dut/aData
+add wave -noupdate -radix decimal /MMM_tb/dut/bData
+add wave -noupdate -divider WATCH
+add wave -noupdate -radix decimal /MMM_tb/dut/mac/product
+add wave -noupdate -radix decimal /MMM_tb/dut/mac/add1
+add wave -noupdate -radix decimal /MMM_tb/dut/mac/sum
+add wave -noupdate -radix decimal /MMM_tb/dut/macOut
+add wave -noupdate -radix decimal /MMM_tb/dut/clearAcc
+add wave -noupdate -divider -height 25 {OTHER DATA}
+add wave -noupdate /MMM_tb/dut/matricesLoaded
+add wave -noupdate /MMM_tb/dut/computeFinished
+add wave -noupdate /MMM_tb/dut/validInput
+add wave -noupdate /MMM_tb/dut/index
+add wave -noupdate /MMM_tb/dut/outputCol
+add wave -noupdate /MMM_tb/dut/outputRow
+add wave -noupdate -divider -height 25 {Delay Registers}
+add wave -noupdate /MMM_tb/dut/clearAccDelay1
+add wave -noupdate /MMM_tb/dut/clearAccDelay2
+add wave -noupdate /MMM_tb/dut/fifoWriteEnableDelay1
+add wave -noupdate /MMM_tb/dut/fifoWriteEnableDelay2
+add wave -noupdate /MMM_tb/dut/validDelay1
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 3} {475 ns} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 335
+configure wave -valuecolwidth 90
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {334 ns} {598 ns}
